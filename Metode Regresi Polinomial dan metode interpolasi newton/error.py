2872f6887e3999b375c9c76a36e294803149bab0
Wildan2701 <86366174+Wildan2701@users.noreply.github.com>
2022-10-29 21:08:07 +0700

    Add files via upload

---
+++ Metode Regresi Polinomial dan metode interpolasi newton/error.py
@@ -0,0 +1,5 @@
+import sys
+def err(string):
+    print(string)
+    raw_input('Press return to exit')
+    sys.exit()
---
+++ Metode Regresi Polinomial dan metode interpolasi newton/newtonPoly.py
@@ -0,0 +1,21 @@
+## module newtonPoly
+''' p = evalPoly(a,xData,x).
+    Menghitung Polinomial Newton p saat x. Koefisien
+    vektor 'a' dapat dihitung dengan fungsi 'coeffts'.
+
+    a = coeffts(xData,yData).
+    Menghitung koefisien polinom Newton.
+'''
+def evalPoly(a,xData,x):
+    n = len(xData) - 1 # Degree of polynomial
+    p = a[n]
+    for k in range(1,n+1):
+        p = a[n-k] + (x -xData[n-k])*p
+    return p
+
+def coeffts(xData,yData):
+    m = len(xData) # Number of data points
+    a = yData.copy()
+    for k in range(1,m):
+        a[k:m] = (a[k:m] - a[k-1])/(xData[k:m] - xData[k-1])
+        return a
